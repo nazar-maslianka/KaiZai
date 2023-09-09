9fe43b8dd282b52bd9fa943f0e3dd92640558ec7
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2023-09-09 17:37:58 +0300

    KaiZai.Service.Categories.API | Updated Category

--- src/Services/KaiZai.Service.Categories/KaiZai.Service.Categories.API/Data/Entities/Category.cs
+++ src/Services/KaiZai.Service.Categories/KaiZai.Service.Categories.API/Data/Entities/Category.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using KaiZai.Service.Categories.API.Data.Enums;
 
 namespace KaiZai.Service.Categories.API.Data.Entities;
@@ -5,7 +6,9 @@ namespace KaiZai.Service.Categories.API.Data.Entities;
 public sealed class Category : IEntity
 {
     public Guid Id { get; set; }
+    [Required]
     public Guid UserId { get; set; }
+    [Required]
     public string Name { get; set; }
     public CategoryType CategoryType { get; set; }
 }
