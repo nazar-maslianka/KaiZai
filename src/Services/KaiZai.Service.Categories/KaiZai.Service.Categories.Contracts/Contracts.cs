a8c7d4d39791a6df974c51d9eb1c8d6fcda70981
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2023-09-11 15:12:03 +0300

    KaiZai.Service.Categories | Add reference on KaiZai.Common.Types in Contracts project

--- src/Services/KaiZai.Service.Categories/KaiZai.Service.Categories.Contracts/Contracts.cs
+++ src/Services/KaiZai.Service.Categories/KaiZai.Service.Categories.Contracts/Contracts.cs
@@ -1,5 +1,8 @@
+using KaiZai.Common.Types;
+
 namespace KaiZai.Service.Categories.Contracts;
 
-public record CategoryCreated(Guid Id, Guid ProfileId, string Name);
+public record CategoryCreated(Guid Id, Guid ProfileId, string Name, CategoryType CategoryType);
+//TODO: later
 //public record CatalogItemUpdated(Guid ItemId, string Name, string Description);
 //public record CatalogItemDeleted(Guid ItemId);
