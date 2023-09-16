22fbdc98fe11e6a5dbe1fc5081e35aa8607b929b
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2023-09-16 22:22:25 +0300

    KaiZai.Service.Categories | Add logger in API

--- src/Services/KaiZai.Service.Categories/KaiZai.Service.Categories.API/Program.cs
+++ src/Services/KaiZai.Service.Categories/KaiZai.Service.Categories.API/Program.cs
@@ -1,14 +1,21 @@
 using KaiZai.Service.Categories.API.Extensions;
 using KaiZai.Service.Common.MessageExchangeBaseConfigurator.MassTransit;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Logging.AddSerilog(logger);
 builder.Services.AddMongoDatabase();
 builder.Services.AddMassTransitWithRabbitMq();
 builder.Services.AddRepositories();
 builder.Services.AddFilters();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,12 +26,17 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(s => 
+    {
+        s.SwaggerEndpoint("/swagger/v1/swagger.json", "KaiZai Categories API v1");
+    });
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseSerilogRequestLogging();
 
 app.MapControllers();
 
