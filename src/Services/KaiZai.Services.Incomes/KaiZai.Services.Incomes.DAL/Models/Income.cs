e0f0175de586c1e8c8104b348b4979fd9b1a56ae
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2023-09-19 15:35:43 +0300

    KaiZai.Services.Incomes | Add DAL project

---
+++ src/Services/KaiZai.Services.Incomes/KaiZai.Services.Incomes.DAL/Models/Income.cs
@@ -0,0 +1,13 @@
+using KaiZai.MongoDataAccessAbstraction.Repository;
+
+namespace KaiZai.Services.Incomes.DAL.Models;
+
+public class Income : IEntity
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; } 
+    public Guid CategoryId { get; set; } 
+    public DateTimeOffset IncomeDate { get; set; }
+    public string Description { get; set; }
+    public decimal Amount { get; set; }
+}
