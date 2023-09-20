3b0f1107d5781a4885ec935db9faca86e437fade
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2023-09-20 18:12:32 +0300

    Services.Common |  Minor changes in KaiZai.Service.Common.MongoDataAccessRepository

--- src/Services.Common/KaiZai.Service.Common.MongoDataAccessRepository/Extensions.cs
+++ src/Services.Common/KaiZai.Service.Common.MongoDataAccessRepository/Extensions.cs
@@ -13,7 +13,7 @@ public static class Extensions
     ///</summary>
     /// <param name="serviceSettings">Settings with service name for Mongo collection</param>
     /// <param name="mongoConnectionSettings">Settings for connecting to Mongo database</param>
-    public static void AddMongoDatabase(this IServiceCollection services, ServiceSettings serviceSettings, MongoConnectionSettings mongoConnectionSettings)
+    public static IServiceCollection AddMongoDatabase(this IServiceCollection services, ServiceSettings serviceSettings, MongoConnectionSettings mongoConnectionSettings)
     {
         BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
         BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
@@ -22,5 +22,6 @@ public static class Extensions
             var mongoClient = new MongoClient(mongoConnectionSettings.ConnectionString);
             return mongoClient.GetDatabase(serviceSettings.ServiceName);
         });
+        return services;
     }
 }
