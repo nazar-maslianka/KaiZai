7389e45186d4f9ca81822d2b18b8ef777513fe5b
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2023-09-20 22:32:00 +0300

    KaiZai.Service.Categories | Fixed names and minor changes in UnitTests

--- src/Services/KaiZai.Service.Categories/KaiZai.Service.Categories.UnitTests/DbFixture.cs
+++ src/Services/KaiZai.Service.Categories/KaiZai.Service.Categories.UnitTests/DbFixture.cs
@@ -1,5 +1,3 @@
-using KaiZai.Service.Categories.API.Data.Entities;
-using KaiZai.Service.Categories.API.Data.Enums;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
@@ -45,28 +43,28 @@ public sealed class DbFixture : IDisposable
             new Category
             {
                 Id = new Guid("373b5c56-df08-4774-b096-1e2bba1c0b36"),
-                UserId = new Guid("fb12fd97-e08a-4793-8a17-f9bd70233749"),
+                ProfileId = new Guid("fb12fd97-e08a-4793-8a17-f9bd70233749"),
                 Name = "Test_Groceries",
                 CategoryType = CategoryType.Expense
             },
             new Category
             {
                 Id = new Guid("3eb77f9e-85ff-4794-8a0b-7e48307326ee"),
-                UserId = new Guid("fb12fd97-e08a-4793-8a17-f9bd70233749"),
+                ProfileId = new Guid("fb12fd97-e08a-4793-8a17-f9bd70233749"),
                 Name = "Test_Cafe",
                 CategoryType = CategoryType.Expense
             },
             new Category
             {
                 Id = new Guid("a44817bd-1099-4f34-a02c-1f31d9794f36"),
-                UserId = new Guid("208b0ac2-a0ca-438c-a5e5-fa7ab4dfa10a"),
+                ProfileId = new Guid("208b0ac2-a0ca-438c-a5e5-fa7ab4dfa10a"),
                 Name = "Test_Salary",
                 CategoryType = CategoryType.Income
             },
             new Category
             {
                 Id = new Guid("1f67ab91-a949-4ae4-9b1c-35f65edfa60f"),
-                UserId = new Guid("e106b3d6-a418-4569-afbb-ff680b41f7a0"),
+                ProfileId = new Guid("e106b3d6-a418-4569-afbb-ff680b41f7a0"),
                 Name = "Test_Transportation",
                 CategoryType = CategoryType.Expense
             }
--- src/Services/KaiZai.Service.Categories/KaiZai.Service.Categories.UnitTests/Usings.cs
+++ src/Services/KaiZai.Service.Categories/KaiZai.Service.Categories.UnitTests/Usings.cs
@@ -1,1 +1,3 @@
 global using Xunit;
+global using KaiZai.Common.Types;
+global using KaiZai.Service.Categories.API.Data.Entities;
