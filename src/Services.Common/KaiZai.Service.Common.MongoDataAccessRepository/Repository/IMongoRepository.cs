a87416e021267ce09295b7d6886a6ed3248343c7
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2023-09-30 23:04:17 +0300

    Services.Common | Refactored MongoRepository and IMongoRepository in KaiZai.Service.Common.MongoDataAccessRepository (package updated v1.0.5 -> 1.0.6)

--- src/Services.Common/KaiZai.Service.Common.MongoDataAccessRepository/Repository/IMongoRepository.cs
+++ src/Services.Common/KaiZai.Service.Common.MongoDataAccessRepository/Repository/IMongoRepository.cs
@@ -2,22 +2,149 @@ namespace KaiZai.Service.Common.MongoDataAccessRepository.Repository;
 
 public interface IMongoRepository<TDocument> where TDocument: IEntity
 {
-    Task CreateAsync(TDocument entity);
-    void Create(TDocument entity);
-    Task<ReplaceOneResult?> UpdateAsync(TDocument entity);
-    ReplaceOneResult? Update(TDocument entity);
-    Task<DeleteResult?> RemoveAsync(Guid id);
-    DeleteResult? Remove(Guid id);
+    #region Get operations
+    /// <summary>
+    /// Asynchronously retrieves all documents from the collection.
+    /// </summary>
+    /// <returns>
+    /// A task that represents the asynchronous operation.
+    /// The task result is an <see cref="IReadOnlyCollection{TDocument}"/> containing all documents in the collection.
+    /// </returns>
     Task<IReadOnlyCollection<TDocument>> GetAllAsync();
+
+    /// <summary>
+    /// Synchronously retrieves all documents from the collection.
+    /// </summary>
+    /// <returns>An <see cref="IReadOnlyCollection{TDocument}"/> containing all documents in the collection.</returns>
     IReadOnlyCollection<TDocument> GetAll();
+
+    /// <summary>
+    /// Asynchronously retrieves all documents from the collection that match a specified filter expression.
+    /// </summary>
+    /// <param name="filter">The filter expression to apply when retrieving documents.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation.
+    /// The task result is an <see cref="IReadOnlyCollection{TDocument}"/> containing matching documents.
+    /// </returns>
     Task<IReadOnlyCollection<TDocument>> GetAllAsync(Expression<Func<TDocument, bool>> filter);
-    IReadOnlyCollection<TDocument> GetAll(Expression<Func<TDocument, bool>> filter);
+
+    /// <summary>
+    /// Gets an aggregate query builder for performing aggregation operations on the collection.
+    /// </summary>
+    /// <param name="filterDefinition">
+    /// An optional filter definition to filter the documents before aggregation.
+    /// If not provided, the aggregation will operate on all documents in the collection.
+    /// </param>
+    /// <returns>
+    /// An <see cref="IAggregateFluent{TDocument}"/> representing the aggregate query builder.
+    /// </returns>
+    IAggregateFluent<TDocument>? GetBaseAggregateQuery(Expression<Func<TDocument, bool>>? filterDefinition);
+
+    /// <summary>
+    /// Asynchronously performs aggregation on a collection by page, with the specified page size and number.
+    /// </summary>
+    /// <param name="pageSize">
+    /// The maximum number of documents to include in each page of the aggregation results.
+    /// </param>
+    /// <param name="pageNumber">
+    /// The page number, indicating which page of the aggregation results to retrieve.
+    /// </param>
+    /// <param name="aggregation">
+    /// The aggregation query to execute on the collection.
+    /// </param>
+    /// <returns>
+    /// A task that represents the asynchronous operation.
+    /// The task result is a <see cref="RepositoryAggregationResult{TDocument}"/> containing the aggregation results for the specified page.
+    /// </returns>
     Task<RepositoryAggregationResult<TDocument>> GetAggregateByPageAsync(
-        Expression<Func<TDocument, bool>>? filterDefinition,
-        FieldDefinition<TDocument>? sortDefinitionField,
-        int pageNumber, int pageSize);
+        int pageSize,
+        int pageNumber,
+        IAggregateFluent<TDocument> aggregation);
+    
+    /// <summary>
+    /// Asynchronously retrieves a single document from the collection by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the document to retrieve.</param>
+    /// <returns>
+    /// A task representing the asynchronous operation.
+    /// The task result is the retrieved document or null if not found.
+    /// </returns>
     Task<TDocument> GetOneAsync(Guid id);
+
+    /// <summary>
+    /// Synchronously retrieves a single document from the collection by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the document to retrieve.</param>
+    /// <returns>
+    /// The retrieved document or null if not found.
+    /// </returns>
     TDocument GetOne(Guid id);
+
+    /// <summary>
+    /// Asynchronously retrieves a single document from the collection that matches a specified filter expression.
+    /// </summary>
+    /// <param name="filter">The filter expression to apply when retrieving the document.</param>
+    /// <returns>
+    /// A task representing the asynchronous operation.
+    /// The task result is the retrieved document or null if not found.
+    /// </returns>
     Task<TDocument?> GetOneAsync(Expression<Func<TDocument, bool>> filter);
+    
+    /// <summary>
+    /// Synchronously retrieves a single document from the collection that matches a specified filter expression.
+    /// </summary>
+    /// <param name="filter">The filter expression to apply when retrieving the document.</param>
+    /// <returns>
+    /// The retrieved document or null if not found.
+    /// </returns>
     TDocument? GetOne(Expression<Func<TDocument, bool>> filter);
+    #endregion
+
+    #region CRUD operations
+    /// <summary>
+    /// Asynchronously creates a new document in the collection.
+    /// </summary>
+    /// <param name="entity">The document entity to be created.</param>
+    Task CreateAsync(TDocument entity);
+
+    /// <summary>
+    /// Synchronously creates a new document in the collection.
+    /// </summary>
+    /// <param name="entity">The document entity to be created.</param>
+    void Create(TDocument entity);
+
+    /// <summary>
+    /// Asynchronously updates an existing document in the collection.
+    /// </summary>
+    /// <param name="entity">The document entity with updated data.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation.
+    /// The task result is a <see cref="ReplaceOneResult"/> representing the update result.
+    /// </returns>
+    Task<ReplaceOneResult?> UpdateAsync(TDocument entity);
+
+    /// <summary>
+    /// Synchronously updates an existing document in the collection.
+    /// </summary>
+    /// <param name="entity">The document entity with updated data.</param>
+    /// <returns>A <see cref="ReplaceOneResult"/> representing the update result.</returns>
+    ReplaceOneResult? Update(TDocument entity);
+
+    /// <summary>
+    /// Asynchronously removes a document from the collection by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the document to be removed.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation.
+    /// The task result is a <see cref="DeleteResult"/> representing the removal result.
+    /// </returns>
+    Task<DeleteResult?> RemoveAsync(Guid id);
+
+    /// <summary>
+    /// Synchronously removes a document from the collection by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the document to be removed.</param>
+    /// <returns>A <see cref="DeleteResult"/> representing the removal result.</returns>
+    DeleteResult? Remove(Guid id);
+    #endregion
 }
--- src/Services.Common/KaiZai.Service.Common.MongoDataAccessRepository/Repository/MongoRepository.cs
+++ src/Services.Common/KaiZai.Service.Common.MongoDataAccessRepository/Repository/MongoRepository.cs
@@ -1,28 +1,53 @@
 namespace KaiZai.Service.Common.MongoDataAccessRepository.Repository;
 
-///<summary>
-///An abstract repository class for access mongo collections.
-///</summary>
+/// <summary>
+/// Represents a base class for MongoDB repositories that provides common functionality for data access.
+/// </summary>
+/// <typeparam name="TDocument">The type of documents stored in the MongoDB collection.</typeparam>
 public abstract class MongoRepository<TDocument> : IMongoRepository<TDocument> where TDocument : IEntity
 {
+ /// <summary>
+    /// The MongoDB collection used for data access.
+    /// </summary>
     protected readonly IMongoCollection<TDocument> _dbCollection;
+    
+    /// <summary>
+    /// The filter builder for creating MongoDB filter definitions.
+    /// </summary>
     protected readonly FilterDefinitionBuilder<TDocument> _filterBuilder = Builders<TDocument>.Filter;
+    
+    /// <summary>
+    /// The sort builder for creating MongoDB sort definitions.
+    /// </summary>
     protected readonly SortDefinitionBuilder<TDocument> _sortBuilder = Builders<TDocument>.Sort;
 
-    ///<summary>
-    ///Initialize access to mongo collection.
-    ///</summary>
-    /// <param name="mongoDatabase">Instance of mongo database. If null, returns ArgumentNullException</param>
-    /// <param name="collectionName">Name of mongo collection. If null or empty, returns ArgumentNullException</param>
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MongoRepository{TDocument}"/> class.
+    /// </summary>
+    /// <param name="mongoDatabase">The MongoDB database to use for data access.</param>
+    /// <param name="collectionName">The name of the collection within the database.</param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="mongoDatabase"/> or <paramref name="collectionName"/> is null or empty.
+    /// </exception>
     public MongoRepository(IMongoDatabase mongoDatabase, string collectionName)
     {
-        if (mongoDatabase == null) throw new ArgumentNullException($"Parameter: {nameof(mongoDatabase)} in {nameof(MongoRepository<TDocument>)} is null!");
-        if (string.IsNullOrEmpty(collectionName)) throw new ArgumentNullException($"Parameter: {nameof(collectionName)} in {nameof(MongoRepository<TDocument>)} is null or empty!");
+        if (mongoDatabase == null)
+            throw new ArgumentNullException($"{nameof(mongoDatabase)} parameter in {nameof(MongoRepository<TDocument>)} is null!");
+
+        if (string.IsNullOrEmpty(collectionName))
+            throw new ArgumentNullException($"{nameof(collectionName)} parameter in {nameof(MongoRepository<TDocument>)} is null or empty!");
 
         _dbCollection = mongoDatabase.GetCollection<TDocument>(collectionName);
     }
    
     #region Get operations
+    /// <summary>
+    /// Asynchronously retrieves all documents from the collection.
+    /// </summary>
+    /// <returns>
+    /// A task that represents the asynchronous operation.
+    /// The task result is an <see cref="IReadOnlyCollection{TDocument}"/> containing all documents in the collection.
+    /// </returns>
     public async Task<IReadOnlyCollection<TDocument>> GetAllAsync()
     {
         var documentsList = await _dbCollection
@@ -30,6 +55,11 @@ public abstract class MongoRepository<TDocument> : IMongoRepository<TDocument> w
             .ToListAsync();
         return documentsList.AsReadOnly();
     }
+
+    /// <summary>
+    /// Synchronously retrieves all documents from the collection.
+    /// </summary>
+    /// <returns>An <see cref="IReadOnlyCollection{TDocument}"/> containing all documents in the collection.</returns>
     public IReadOnlyCollection<TDocument> GetAll()
     {
         return _dbCollection
@@ -37,6 +67,15 @@ public abstract class MongoRepository<TDocument> : IMongoRepository<TDocument> w
             .ToList()
             .AsReadOnly();
     }
+
+    /// <summary>
+    /// Asynchronously retrieves all documents from the collection that match a specified filter expression.
+    /// </summary>
+    /// <param name="filter">The filter expression to apply when retrieving documents.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation.
+    /// The task result is an <see cref="IReadOnlyCollection{TDocument}"/> containing matching documents.
+    /// </returns>
     public virtual async Task<IReadOnlyCollection<TDocument>> GetAllAsync(Expression<Func<TDocument, bool>> filter)
     {
         if (filter == null)
@@ -44,12 +83,21 @@ public abstract class MongoRepository<TDocument> : IMongoRepository<TDocument> w
             return new List<TDocument>()
                 .AsReadOnly();
         }
-        
+
         var documentsList = await _dbCollection
             .Find(filter)
             .ToListAsync();
         return documentsList.AsReadOnly();
     }
+
+    /// <summary>
+    /// Synchronously retrieves all documents from the collection that match a specified filter expression.
+    /// </summary>
+    /// <param name="filter">The filter expression to apply when retrieving documents.</param>
+    /// <returns>
+    /// An <see cref="IReadOnlyCollection{TDocument}"/> containing matching documents.
+    /// If the filter is null, an empty collection is returned.
+    /// </returns>
     public virtual IReadOnlyCollection<TDocument> GetAll(Expression<Func<TDocument, bool>> filter)
     {
         if (filter == null)
@@ -57,26 +105,52 @@ public abstract class MongoRepository<TDocument> : IMongoRepository<TDocument> w
             return new List<TDocument>()
                 .AsReadOnly();
         }
-        
+
         return _dbCollection
             .Find(filter)
             .ToList()
             .AsReadOnly();
     }
+
+    /// <summary>
+    /// Asynchronously retrieves a single document from the collection by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the document to retrieve.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation.
+    /// The task result is the retrieved document or null if not found.
+    /// </returns>
     public async Task<TDocument> GetOneAsync(Guid id)
     {
-        FilterDefinition<TDocument> filter =_filterBuilder.Eq(entity => entity.Id, id);
+        FilterDefinition<TDocument> filter = _filterBuilder.Eq(entity => entity.Id, id);
         return await _dbCollection
             .Find(filter)
             .FirstOrDefaultAsync();
     }
+
+    /// <summary>
+    /// Synchronously retrieves a single document from the collection by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the document to retrieve.</param>
+    /// <returns>
+    /// The retrieved document or null if not found.
+    /// </returns>
     public TDocument GetOne(Guid id)
     {
-        FilterDefinition<TDocument> filter =_filterBuilder.Eq(entity => entity.Id, id);
+        FilterDefinition<TDocument> filter = _filterBuilder.Eq(entity => entity.Id, id);
         return _dbCollection
             .Find(filter)
             .FirstOrDefault();
     }
+
+    /// <summary>
+    /// Asynchronously retrieves a single document from the collection that matches a specified filter expression.
+    /// </summary>
+    /// <param name="filter">The filter expression to apply when retrieving the document.</param>
+    /// <returns>
+    /// A task that represents the asynchronous operation.
+    /// The task result is the retrieved document or null if not found.
+    /// </returns>
     public virtual async Task<TDocument?> GetOneAsync(Expression<Func<TDocument, bool>> filter)
     {
         if (filter == null)
@@ -87,6 +161,14 @@ public abstract class MongoRepository<TDocument> : IMongoRepository<TDocument> w
             .Find(filter)
             .FirstOrDefaultAsync();
     }
+
+    /// <summary>
+    /// Synchronously retrieves a single document from the collection that matches a specified filter expression.
+    /// </summary>
+    /// <param name="filter">The filter expression to apply when retrieving the document.</param>
+    /// <returns>
+    /// The retrieved document or null if not found.
+    /// </returns>
     public virtual TDocument? GetOne(Expression<Func<TDocument, bool>> filter)
     {
         if (filter == null)
@@ -98,16 +180,44 @@ public abstract class MongoRepository<TDocument> : IMongoRepository<TDocument> w
             .FirstOrDefault();
     }
     
-    /// <summary> 
-    ///Returns filtered, sorted and paged documents and total count of elements in database collection
-    ///</summary>
-    /// <param name="filterDefinition"> Defines logic for filtering. Could be null</param>
-    /// <param name="sortDefinitionField"> Defines field of class for sorting. Could be null </param>
-    /// <param name="pagingParams"> Settings for skip and limit pages<
+    /// <summary>
+    /// Gets an aggregate query builder for performing aggregation operations on the collection.
+    /// </summary>
+    /// <param name="filterDefinition">
+    /// An optional filter definition to filter the documents before aggregation.
+    /// If not provided, the aggregation will operate on all documents in the collection.
+    /// </param>
+    /// <returns>
+    /// An <see cref="IAggregateFluent{TDocument}"/> representing the aggregate query builder.
+    /// </returns>
+    public virtual IAggregateFluent<TDocument>? GetBaseAggregateQuery(Expression<Func<TDocument, bool>>? filterDefinition) 
+    {
+        return _dbCollection.Aggregate()
+            .Match(filterDefinition != null
+                ? _filterBuilder.Where(filterDefinition)
+                : _filterBuilder.Empty);
+    }
+
+    /// <summary>
+    /// Asynchronously performs aggregation on a collection by page, with the specified page size and number.
+    /// </summary>
+    /// <param name="pageSize">
+    /// The maximum number of documents to include in each page of the aggregation results.
+    /// </param>
+    /// <param name="pageNumber">
+    /// The page number, indicating which page of the aggregation results to retrieve.
+    /// </param>
+    /// <param name="aggregation">
+    /// The aggregation query to execute on the collection.
+    /// </param>
+    /// <returns>
+    /// A task that represents the asynchronous operation.
+    /// The task result is a <see cref="RepositoryAggregationResult{TDocument}"/> containing the aggregation results for the specified page.
+    /// </returns>
     public virtual async Task<RepositoryAggregationResult<TDocument>> GetAggregateByPageAsync(
-        Expression<Func<TDocument, bool>>? filterDefinition, 
-        FieldDefinition<TDocument>? sortDefinitionField, 
-        int pageNumber, int pageSize)
+        int pageSize,
+        int pageNumber, 
+        IAggregateFluent<TDocument> aggregation)
     {
         var countFacet  = AggregateFacet.Create(Facets.Count,
             PipelineDefinition<TDocument, AggregateCountResult>.Create(new[]
@@ -119,22 +229,19 @@ public abstract class MongoRepository<TDocument> : IMongoRepository<TDocument> w
             PipelineDefinition<TDocument, TDocument>.Create(new[]
             {
                 PipelineStageDefinitionBuilder.Skip<TDocument>((pageNumber - 1) * pageSize),
-                PipelineStageDefinitionBuilder.Limit<TDocument>(pageSize),
+                PipelineStageDefinitionBuilder.Limit<TDocument>(pageSize)
             }));
-
-        var aggregation = await  _dbCollection.Aggregate()
-              .Match(filterDefinition != null 
-                  ? _filterBuilder.Where(filterDefinition)
-                  : _filterBuilder.Empty)
-              .Facet(countFacet, dataFacet)
-              .ToListAsync();
-            
-        var count = (int?) aggregation.First().Facets
+        
+        var aggregationWithFacets = await aggregation
+            .Facet(countFacet, dataFacet)
+            .ToListAsync();
+                  
+        var count = (int?) aggregationWithFacets.First().Facets
             .First(x => x.Name == Facets.Count)
             .Output<AggregateCountResult>().FirstOrDefault()?
             .Count;
         
-        var data = aggregation.First()
+        var data = aggregationWithFacets.First()
             .Facets.First(x => x.Name == Facets.Data)
             .Output<TDocument>();
                 
@@ -143,6 +250,11 @@ public abstract class MongoRepository<TDocument> : IMongoRepository<TDocument> w
     #endregion
 
     #region CRUD operations
+    /// <summary>
+    /// Asynchronously creates a new document in the collection.
+    /// </summary>
+    /// <param name="entity">The document entity to be created.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     public virtual async Task CreateAsync(TDocument entity)
     {
         if (entity != null)
@@ -150,6 +262,11 @@ public abstract class MongoRepository<TDocument> : IMongoRepository<TDocument> w
             await _dbCollection.InsertOneAsync(entity);
         }
     }
+
+    /// <summary>
+    /// Synchronously creates a new document in the collection.
+    /// </summary>
+    /// <param name="entity">The document entity to be created.</param>
     public virtual void Create(TDocument entity)
     {
         if (entity != null)
@@ -157,32 +274,66 @@ public abstract class MongoRepository<TDocument> : IMongoRepository<TDocument> w
             _dbCollection.InsertOne(entity);
         }
     }
+
+    /// <summary>
+    /// Asynchronously updates an existing document in the collection.
+    /// </summary>
+    /// <param name="entity">The document entity with updated data.</param>
+    /// <returns>
+    /// A task representing the asynchronous operation.
+    /// The task result is a <see cref="ReplaceOneResult"/> representing the update result.
+    /// </returns>
     public virtual async Task<ReplaceOneResult?> UpdateAsync(TDocument entity)
     {
         if (entity != null)
         {
-            FilterDefinition<TDocument> filter =_filterBuilder.Eq(x => x.Id, entity.Id);
+            FilterDefinition<TDocument> filter = _filterBuilder.Eq(x => x.Id, entity.Id);
             return await _dbCollection.ReplaceOneAsync(filter, entity); 
         }
         return null;
     }
+
+    /// <summary>
+    /// Synchronously updates an existing document in the collection.
+    /// </summary>
+    /// <param name="entity">The document entity with updated data.</param>
+    /// <returns>
+    /// A <see cref="ReplaceOneResult"/> representing the update result.
+    /// </returns>
     public virtual ReplaceOneResult? Update(TDocument entity)
     {
         if (entity != null)
         {
-            FilterDefinition<TDocument> filter =_filterBuilder.Eq(x => x.Id, entity.Id);
+            FilterDefinition<TDocument> filter = _filterBuilder.Eq(x => x.Id, entity.Id);
             return _dbCollection.ReplaceOne(filter, entity); 
         }
         return null;
     }
+
+    /// <summary>
+    /// Asynchronously removes a document from the collection by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the document to be removed.</param>
+    /// <returns>
+    /// A task representing the asynchronous operation.
+    /// The task result is a <see cref="DeleteResult"/> representing the removal result.
+    /// </returns>
     public virtual async Task<DeleteResult?> RemoveAsync(Guid id)
     {
-        FilterDefinition<TDocument> filter =_filterBuilder.Eq(entity => entity.Id, id);
+        FilterDefinition<TDocument> filter = _filterBuilder.Eq(entity => entity.Id, id);
         return await _dbCollection.DeleteOneAsync(filter);   
     }
+
+    /// <summary>
+    /// Synchronously removes a document from the collection by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the document to be removed.</param>
+    /// <returns>
+    /// A <see cref="DeleteResult"/> representing the removal result.
+    /// </returns>
     public virtual DeleteResult? Remove(Guid id)
     {
-        FilterDefinition<TDocument> filter =_filterBuilder.Eq(entity => entity.Id, id);
+        FilterDefinition<TDocument> filter = _filterBuilder.Eq(entity => entity.Id, id);
         return _dbCollection.DeleteOne(filter);  
     }
     #endregion
