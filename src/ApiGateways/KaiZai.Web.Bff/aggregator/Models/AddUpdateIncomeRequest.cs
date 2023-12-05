ea449cfd7a421c555c602142b9b385be12fd32bf
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2023-12-05 19:17:02 +0200

    KaiZai.Web.Bff | HttpAggregator | Added records in Models for Income Service

--- src/ApiGateways/KaiZai.Web.Bff/aggregator/Models/AddUpdateIncomeRequest.cs
+++ src/ApiGateways/KaiZai.Web.Bff/aggregator/Models/AddUpdateIncomeRequest.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace KaiZai.Web.HttpAggregator.Models;
 
 public sealed record AddUpdateIncomeRequest (
