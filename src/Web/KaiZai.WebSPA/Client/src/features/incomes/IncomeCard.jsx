725c51f33c994b53cf740a887d18bf746fdbd9d7
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2024-02-13 13:26:42 +0200

    KaiZai.Web.Bff | Removed test features

--- src/Web/KaiZai.WebSPA/Client/src/features/incomes/IncomeCard.jsx
+++
@@ -1,9 +0,0 @@
-import agent from "../../app/api/agent";
-
-export default function IncomeCard() {
-    const params = new URLSearchParams();
-    params.append('pageNumber', '2');
-    params.append('pageSize', '3');
-    const res = agent.Incomes.list(params);
-    console.log(res);
-}
