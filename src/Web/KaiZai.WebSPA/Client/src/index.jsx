edd9c57dac20be98ce66abf73ef28d95dee0a6c9
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2024-03-07 12:57:55 +0200

    KaiZai.WebSPA | Added craco for Semantic UI and recharts, some changes in styles

--- src/Web/KaiZai.WebSPA/Client/src/index.jsx
+++ src/Web/KaiZai.WebSPA/Client/src/index.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/layout/App.jsx'
 import './index.css'
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-less/semantic.less'
 import { Provider } from 'react-redux'
 import { store } from  './app/stores/configureStore'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
       <Provider store={store}>
         <App />
       </Provider>
-  </React.StrictMode>
 )
